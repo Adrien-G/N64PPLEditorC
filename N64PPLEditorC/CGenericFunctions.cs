57f287f56ad27f2dbd83243eaaa2c9d31218a524
Adrien <Adrien@DESKTOP-ADRIEN>
2020-02-10 17:21:16 +0100

    adding generic function for verifying application path and a convertByteArrayToInt function.

--- N64PPLEditorC/CGenericFunctions.cs
+++ N64PPLEditorC/CGenericFunctions.cs
@@ -3,11 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
 
 namespace N64PPLEditorC
 {
-    public static class CGenericFunctions
+    public class CGenericFunctions
     {
 
+        public static String pathExtractedTexture = Application.StartupPath + @"\extractedTexture\";
+        public static String pathOtherContent = Application.StartupPath + @"\OtherContent\";
+        public static String pathCompressedTexture = Application.StartupPath + @"\compressedTexture\";
+        public static String pathReplacedTexture = Application.StartupPath + @"\replacedTexture\";
+
+
+        public static Int32 ConvertByteArrayToInt(Byte[] byteArray)
+        {
+            if (BitConverter.IsLittleEndian)
+                Array.Reverse(byteArray);
+
+            return BitConverter.ToInt32(byteArray, 0);
+        }
+
+        public static void VerifyExistingPath()
+        {
+            if (!File.Exists(pathExtractedTexture))
+            {
+                Directory.CreateDirectory(pathExtractedTexture);
+            }
+            if (!File.Exists(pathOtherContent))
+            {
+                Directory.CreateDirectory(pathOtherContent);
+            }
+            if (!File.Exists(pathCompressedTexture))
+            {
+                Directory.CreateDirectory(pathCompressedTexture);
+            }
+            if (!File.Exists(pathReplacedTexture))
+            {
+                Directory.CreateDirectory(pathReplacedTexture);
+            }
+        }
     }
 }
