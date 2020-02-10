58f7afc1118bf0cc1fada34b324e7075252b279b
Adrien <Adrien@DESKTOP-ADRIEN>
2020-02-10 16:01:21 +0100

    Ajoutez des fichiers projet.

---
+++ N64PPLEditorC/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace N64PPLEditorC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonLoadRom = new System.Windows.Forms.Button();
+            this.textBoxPPLLocation = new System.Windows.Forms.TextBox();
+            this.buttonGetRomFolder = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.buttonGetRomFolder);
+            this.groupBox1.Controls.Add(this.textBoxPPLLocation);
+            this.groupBox1.Controls.Add(this.buttonLoadRom);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(754, 75);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "PPL location : ";
+            // 
+            // buttonLoadRom
+            // 
+            this.buttonLoadRom.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonLoadRom.Location = new System.Drawing.Point(673, 46);
+            this.buttonLoadRom.Name = "buttonLoadRom";
+            this.buttonLoadRom.Size = new System.Drawing.Size(75, 23);
+            this.buttonLoadRom.TabIndex = 0;
+            this.buttonLoadRom.Text = "Load rom";
+            this.buttonLoadRom.UseVisualStyleBackColor = true;
+            // 
+            // textBoxPPLLocation
+            // 
+            this.textBoxPPLLocation.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxPPLLocation.Location = new System.Drawing.Point(6, 19);
+            this.textBoxPPLLocation.Name = "textBoxPPLLocation";
+            this.textBoxPPLLocation.Size = new System.Drawing.Size(702, 20);
+            this.textBoxPPLLocation.TabIndex = 1;
+            // 
+            // buttonGetRomFolder
+            // 
+            this.buttonGetRomFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonGetRomFolder.Location = new System.Drawing.Point(714, 16);
+            this.buttonGetRomFolder.Name = "buttonGetRomFolder";
+            this.buttonGetRomFolder.Size = new System.Drawing.Size(34, 23);
+            this.buttonGetRomFolder.TabIndex = 1;
+            this.buttonGetRomFolder.Text = "...";
+            this.buttonGetRomFolder.UseVisualStyleBackColor = true;
+            this.buttonGetRomFolder.Click += new System.EventHandler(this.buttonGetRomFolder_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 93);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(312, 680);
+            this.treeView1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(778, 785);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "PPL - Rom Management";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonGetRomFolder;
+        private System.Windows.Forms.TextBox textBoxPPLLocation;
+        private System.Windows.Forms.Button buttonLoadRom;
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
+
---
+++ N64PPLEditorC/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.io
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace N64PPLEditorC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonGetRomFolder_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openfileDialog = new OpenFileDialog())
+            {
+                openfileDialog.InitialDirectory = Application.StartupPath;
+
+            }
+
+        }
+    }
+}
