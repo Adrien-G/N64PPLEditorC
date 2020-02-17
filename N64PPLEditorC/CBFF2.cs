bad1db887349b9c579dea7cf60731ec2ecc924ed
Adrien <adriengarreau@hotmail.fr>
2020-02-17 12:41:12 +0100

    write BFF2 structure header

--- N64PPLEditorC/CBFF2.cs
+++ N64PPLEditorC/CBFF2.cs
@@ -9,8 +9,21 @@ namespace N64PPLEditorC
     class CBFF2
     {
         private Byte[] rawData;
+        
+        //create the BFF2 struct, represent information of the BFF header and also calculated fields.
+        public struct BFFHeader
+        {
+            public int sizeX;
+            public int sizeY;
+            public byte bytePerPixel;
+        }
 
         public CBFF2(Byte[] rawData)
+        {
+            this.rawData = rawData;
+        }
+
+        public void Init()
         {
 
         }
