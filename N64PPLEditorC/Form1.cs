1dd6dd77eac0567f52b7b8b225977819b4f68935
Adrien <adriengarreau@hotmail.fr>
2020-04-18 22:51:19 +0200

    ui modification

--- N64PPLEditorC/Form1.cs
+++ N64PPLEditorC/Form1.cs
@@ -76,8 +76,11 @@ namespace N64PPLEditorC
                     buttonLoadRom.Text = "ROM Loaded";
                     LoadRessourcesList();
                     LoadTreeView();
+                    groupBoxTextures.Enabled = true;
+                    tabControlTexMovSce.Enabled = true;
+                    buttonModifyRom.Enabled = true;
                 }
-                catch (Exception ex)
+               catch (Exception ex)
                 {
                     MessageBox.Show("Error opening rom..." + Environment.NewLine + "error details : " + ex.Message, "PPL Rom management error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 }
@@ -154,20 +157,15 @@ namespace N64PPLEditorC
             this.ressourceList.Init(nbElementsInTable, dataTable, ressourcesData);
         }
 
-        private void buttonShowTexture_Click(object sender, EventArgs e)
-        {
-            if(treeViewTextures.SelectedNode.Level == 1)
-                this.ressourceList.ShowTexture(pictureBoxTexture,treeViewTextures.SelectedNode.Parent.Index,treeViewTextures.SelectedNode.Index);
-        }
-
         private void treeViewTextures_AfterSelect(object sender, TreeViewEventArgs e)
         {
             int level = treeViewTextures.SelectedNode.Level;
-            if (checkBoxAlwaysShowTexture.Checked && level != 0)
+            if (level != 0)
             {
                 labelIsTextureContainer.Hide();
                  this.ressourceList.ShowTexture(pictureBoxTexture, treeViewTextures.SelectedNode.Parent.Index, treeViewTextures.SelectedNode.Index);
                 pictureBoxTexture.Show();
+                numericUpDownTextureDisplayTime.Value = this.ressourceList.GetTextureDisplayTime(treeViewTextures.SelectedNode.Parent.Index, treeViewTextures.SelectedNode.Index);
             }
             if (level == 0)
             {
@@ -238,14 +236,6 @@ namespace N64PPLEditorC
             treeViewSBF.EndUpdate();
         }
 
-        private void checkBoxAlwaysShowTexture_CheckedChanged(object sender, EventArgs e)
-        {
-            if (checkBoxAlwaysShowTexture.Checked)
-                buttonShowTexture.Enabled = false;
-            else
-                buttonShowTexture.Enabled = true;
-        }
-
         private void buttonModifyRom_Click(object sender, EventArgs e)
         {
             ressourceList.WriteAllData(textBoxPPLLocation.Text);
@@ -255,64 +245,82 @@ namespace N64PPLEditorC
         {
             using (OpenFileDialog openTexture = new OpenFileDialog())
             {
-                openTexture.InitialDirectory = Application.StartupPath;
+                //openTexture.InitialDirectory = Application.StartupPath;
                 openTexture.RestoreDirectory = true;
+                openTexture.Multiselect = true;
                 if (openTexture.ShowDialog() == DialogResult.OK)
                 {
                     try
                     {
-                        Image img = Image.FromFile(openTexture.FileName);
-                        if (img.Width <= 320 && img.Height <= 240)
-                        {
-                            //load texture
-                            pictureBoxTexture.Width = img.Width;
-                            pictureBoxTexture.Height = img.Height;
-                            pictureBoxTexture.Image = img;
+                        foreach (String inputTexture in openTexture.FileNames) { 
 
-                            //test the best compression available
-                            var compressionMethod = CTextureManager.TestBestCompression((Bitmap)pictureBoxTexture.Image);
-
-                            //convert texture to byte array for future treatment 
-                            byte[] rawData = CTextureManager.ConvertRGBABitmapToByteArrayRGBA((Bitmap)pictureBoxTexture.Image);
-
-                            //make it at good format
-                            byte[] palette;
-                            (palette, rawData) = CTextureManager.ConvertPixelsToGoodFormat(rawData, compressionMethod);
-
-                            //compress data
-                            byte[] compressedData = CTextureCompress.MakeCompression(rawData);
-
-                            //add header and generate the bff2
-                            string safeFileName = Path.GetFileNameWithoutExtension(openTexture.SafeFileName);
-                            //check if the name has same naming convention than the extract (remove index before coma)
-                            if (safeFileName.Split(',').Count() == 2)
-                                safeFileName = safeFileName.Split(',')[1].Trim();
-
-                            byte[] finalBFF2 = CBFF2.GenerateBFF2(palette, compressedData, compressionMethod, img.Width, img.Height, safeFileName);
-
-                            //add texture to the bff2 files and update treeview
-                            if (treeViewTextures.SelectedNode.Level == 0)
+                            Image img = Image.FromFile(inputTexture);
+                            if (img.Width <= 320 && img.Height <= 240)
                             {
-                                ressourceList.AddBFF2(treeViewTextures.SelectedNode.Index, finalBFF2);
-                                treeViewTextures.SelectedNode.Nodes.Add((treeViewTextures.SelectedNode.Nodes.Count+1) + ", " + safeFileName);
+                                //load texture
+                                pictureBoxTexture.Width = img.Width;
+                                pictureBoxTexture.Height = img.Height;
+                                pictureBoxTexture.Image = img;
+
+                                //test the best compression available
+                                var compressionMethod = CTextureManager.TestBestCompression((Bitmap)pictureBoxTexture.Image);
+
+                                //convert texture to byte array for future treatment 
+                                byte[] rawData = CTextureManager.ConvertRGBABitmapToByteArrayRGBA((Bitmap)pictureBoxTexture.Image);
+
+                                //make it at good format
+                                byte[] palette;
+                                (palette, rawData) = CTextureManager.ConvertPixelsToGoodFormat(rawData, compressionMethod);
+
+                                //compress data
+                                byte[] compressedData = CTextureCompress.MakeCompression(rawData);
+
+                                //add header and generate the bff2
+                                string safeFileName = Path.GetFileNameWithoutExtension(inputTexture);
+                                //check if the name has same naming convention than the extract (remove index before coma)
+                                if (safeFileName.Split(',').Count() == 2)
+                                    safeFileName = safeFileName.Split(',')[1].Trim();
+
+                                byte[] finalBFF2 = CBFF2.GenerateBFF2(palette, compressedData, compressionMethod, img.Width, img.Height, safeFileName);
+
+                                //add texture to the bff2 files and update treeview
+                                if (treeViewTextures.SelectedNode.Level == 0)
+                                {
+                                    ressourceList.AddBFF2(treeViewTextures.SelectedNode.Index, finalBFF2);
+                                    treeViewTextures.SelectedNode.Nodes.Add("[added] , " + safeFileName);
+                                    treeViewTextures.SelectedNode = treeViewTextures.Nodes[treeViewTextures.SelectedNode.Index].LastNode;
+                                }
+                                else
+                                {
+                                    ressourceList.AddBFF2(treeViewTextures.SelectedNode.Parent.Index, finalBFF2);
+                                    treeViewTextures.SelectedNode.Parent.Nodes.Add("[added] , " + safeFileName);
+                                    treeViewTextures.SelectedNode = treeViewTextures.Nodes[treeViewTextures.SelectedNode.Parent.Index].LastNode;
+                                }
                             }
                             else
-                            {
-                                ressourceList.AddBFF2(treeViewTextures.SelectedNode.Parent.Index, finalBFF2);
-                                treeViewTextures.SelectedNode.Parent.Nodes.Add((treeViewTextures.SelectedNode.Nodes.Count + 1) + ", " + safeFileName);
-                            }
+                                MessageBox.Show("Texture must be at maximum of size 320x240 !", "Size too big...", MessageBoxButtons.OK, MessageBoxIcon.Error);
                         }
-                        else
-                            MessageBox.Show("Texture must be at maximum of size 320x240 !", "Size too big...", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    
                     }
                     catch (Exception ex)
                     {
-                        MessageBox.Show("Unrecognized image format :( " + Environment.NewLine + "Error details : " + ex.Message, "Error loading texture", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    MessageBox.Show("Unrecognized image format :( " + Environment.NewLine + "Error details : " + ex.Message, "Error loading texture", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     }
                 }
             }
         }
 
+        private void removeThisTextureToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(treeViewTextures.SelectedNode.Level == 1)
+            {
+                ressourceList.RemoveBFF2(treeViewTextures.SelectedNode.Parent.Index, treeViewTextures.SelectedNode.Index);
+                treeViewTextures.SelectedNode.Remove();
+            }
+            
+        }
+
+        #region helping form
         //part helping on the form..
         private void buttonLoadRom_MouseEnter(object sender, EventArgs e) { helpStatus.Text = "load PPL rom for editing content"; }
         private void buttonGetRomFolder_MouseEnter(object sender, EventArgs e) { helpStatus.Text = "open PPL rom, can only take .z64 file."; }
@@ -321,8 +329,90 @@ namespace N64PPLEditorC
         private void buttonGetRomFolder_MouseLeave(object sender, EventArgs e) { helpStatus.Text = ""; }
         private void treeViewTextures_MouseLeave(object sender, EventArgs e) { helpStatus.Text = ""; }
 
-        private void removeThisTextureToolStripMenuItem_Click(object sender, EventArgs e)
+        #endregion
+
+        private void contextMenuStripForTreeview_Opening(object sender, CancelEventArgs e)
         {
+            if (treeViewTextures.SelectedNode.Level == 1)
+            {
+                removeThisTextureToolStripMenuItem.Enabled = true;
+                containerTypetoolStripMenuItem.Enabled = false;
+            }
+
+            else
+            {
+                removeThisTextureToolStripMenuItem.Enabled = false;
+                containerTypetoolStripMenuItem.Enabled = true;
+            }
+                
+
+        }
+
+        private void treeViewTextures_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            treeViewTextures.SelectedNode = e.Node;
+        }
+
+        private void treeViewTextures_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (treeViewTextures.SelectedNode.Level == 1)
+                if (e.KeyCode == Keys.Delete && treeViewTextures.SelectedNode != null)
+                {
+                    ressourceList.RemoveBFF2(treeViewTextures.SelectedNode.Parent.Index, treeViewTextures.SelectedNode.Index);
+                    treeViewTextures.SelectedNode.Remove();
+                }
+        }
+
+        private void numericUpDownTextureDisplayTime_ValueChanged(object sender, EventArgs e)
+        {
+            this.ressourceList.SetTextureDisplayTime(treeViewTextures.SelectedNode.Parent.Index, treeViewTextures.SelectedNode.Index,(byte)numericUpDownTextureDisplayTime.Value);
+        }
+
+        private void treeViewSBF_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            textBox1.Clear();
+
+            byte[] rawData = ressourceList.GetSBF1(treeViewSBF.SelectedNode.Index).GetRawData();
+
+            bool headerPart = false;
+            int counter = 0;
+
+            for(int i = 0; i < rawData.Length-3; i++)
+            {
+
+                if ((rawData[i] == 0x20 || rawData[i] == 0x21 || rawData[i] == 0x22 || rawData[i] == 0x33 || rawData[i] == 0x62) &&
+                            (rawData[i + 1] == 0x0 || rawData[i + 1] == 0x8 || rawData[i + 1] == 0x80 || rawData[i + 1] == 0x88 || rawData[i + 1] == 0x10) &&
+                            (rawData[i + 2] == 0x0 || rawData[i + 2] == 0x4 || rawData[i + 2] == 0xC4 || rawData[i + 2] == 0xD4 || rawData[i + 2] == 0xC0 || rawData[i + 2] == 0xC || rawData[i + 2] == 0x4C || rawData[i + 2] == 0x14 || rawData[i + 2] == 0x40 || rawData[i + 2] == 0x44) &&
+                            (rawData[i + 3] == 0x3 || rawData[i + 3] == 0x6 || rawData[i + 3] == 0x7 || rawData[i + 3] == 0x42 || rawData[i + 3] == 0x43))
+                {
+                    if(rawData[i] == 0x21 && rawData[i+1] == 0x88 && rawData[i+2] == 0x40 && rawData[i+3] == 0x7)
+                    {
+                        MessageBox.Show("e");
+                    }
+                    headerPart = true;
+                    textBox1.AppendText(Environment.NewLine);
+                    textBox1.AppendText(string.Format("{0,2:X}", rawData[i]) + " ");
+                    textBox1.AppendText(string.Format("{0,2:X}", rawData[i + 1]) + " ");
+                    textBox1.AppendText(string.Format("{0,2:X}", rawData[i + 2]) + " ");
+                    textBox1.AppendText(string.Format("{0,2:X}", rawData[i + 3]) + " ");
+                    i += 3;
+                }
+
+                if (headerPart)
+                    counter++;
+
+                if (counter >= 0x20 && (rawData[i] == 0x0 && rawData[i + 1] == 0x1 && rawData[i + 2] == 0x18) || ((rawData[i] == 0x10 || rawData[i] == 0x14 || rawData[i] == 0x18) && (rawData[i + 1] >= 0x0 && rawData[i + 1] <= 0x3A)))
+                {
+                    if (headerPart)
+                    {
+                        textBox1.AppendText(" -> " + counter);
+                        headerPart = false;
+                        counter = 0;
+                    }
+                    i++;
+                }
+           
+            }
 
         }
     }
